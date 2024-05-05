5c11bb3f3546586a875b39a1d9a28cb2bc86157f
KishoreBabu <kishore.sai37@gmail.com>
2024-05-05 19:03:56 +0530

    Add new components for layout and home page
    * Add `RootLayout` component with sidebar and mobile navigation
    * Add `Sidebars` to the root layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,39 @@
+import HeaderBox from '@/components/headerBox';
+import RightSidebar from '@/components/rightSidebar';
+import TotalBalanceBox from '@/components/totalBalanceBox';
+
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = {
+    firstName: 'Rajesh',
+    lastName: 'Kumar',
+    email: 'rajesh@gmail.com',
+  };
+  return (
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type='greeting'
+            title='Welcome'
+            user={loggedIn?.firstName || 'Guest'}
+            subtext='Access and manage your account and transactions efficiently'
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.45}
+          />
+        </header>
+        Recent Transactions
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 232.5 }]}
+      />
+    </section>
+  );
 };
 
 export default Home;
